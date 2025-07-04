a49c92a376bdc2893ab050594cfa7f84a714fd37
Hanumant Uphade <hanumantuphade63@gmail.com>
2025-07-04 21:37:25 +0530

    Update page.tsx

--- app/bath/page.tsx
+++ app/bath/page.tsx
@@ -232,7 +232,7 @@ export default function BathPage() {
         <div className="max-w-7xl mx-auto px-4 pt-10 pb-16">
           <div className="flex flex-col lg:flex-row gap-8">
             <div className="flex-1">
-              <div className="grid grid-cols-1 px-12 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 md:px-12 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {products.map((product) => (
                   <Link
                     key={product.id}
